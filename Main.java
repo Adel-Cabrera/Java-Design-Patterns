2f420242805e6bf4c491cc01d709ea30be537f16
Darkonnen <adel.cabrera.mesina@gmail.com>
2020-02-18 01:26:27 +0000

    Implemented the Memento pattern with well coded examples.

--- Main.java
+++ Main.java
@@ -24,6 +24,20 @@ class Main {
     drawUIControl(new TextBox());
     drawUIControl(new CheckBox());
 
+    var editor = new Editor();
+    var history = new History();
+
+    editor.setContent("a");
+    history.push(editor.createState());
+
+    editor.setContent("b");
+    history.push(editor.createState());
+
+    editor.setContent("c");
+    editor.restore(history.pop());
+
+    System.out.println(editor.getContent());
+
   }
 
   public static void drawUIControl(UIControl control){
